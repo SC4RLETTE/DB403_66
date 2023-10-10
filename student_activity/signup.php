33ea01fe445c9eee486401a8eb3b10645c8aec80
SC4RLETTE <66111648@dpu.ac.th>
2023-10-10 12:00:23 +0700

    insert login

--- student_activity/signup.php
+++ student_activity/signup.php
@@ -1,5 +1,17 @@
 <?php
-    require 'connect.php';
+  require 'connect.php';
+  if (isset($_POST['submit'])) {
+    $StudentID = $_POST['StudentID'];
+    $StudentName = $_POST['StudentName'];
+    $Major = $_POST['majorID'];
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+    
+    $sql = "insert into 
+      student (studentID, studentName, majorID, password) 
+      values('{$StudentID}','{$StudentName}',
+             '{$Major}','{$password}')";
+    $conn->query($sql);
+  }     
 ?>
 
 <!doctype html>
@@ -24,22 +36,38 @@
       }
     </style>
     
+    <script>
+      function validate() {
+        let p1 = document.querySelector('#password').value;
+        console.log(p1);
+        let p2 = document.querySelector('#re-password').value;
+        console.log(p2);
+        if (p1 != p2) {
+          alert('Password are not identical.');
+          event.preventDefault();
+        }
+      }  
+    </script>
+
   </head>
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main class="form-signup w-100 m-auto">
-      <form>
+
+      <!-- form -->
+      <form action="signup.php" method="post" onsubmit="validate()">
+
         <img class="mb-4" src="image\activity-logo-913-6531.png" alt="" width="300" height="250">
         <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
     
-        <!-- ID -->
+        <!-- Student ID -->
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingEmail" placeholder="">
+          <input required name="StudentID" type="text" class="form-control" id="stuID" placeholder="">
           <label for="student-ID">Student ID</label>
         </div>
 
-        <!-- Name -->
+        <!-- Student Name -->
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingEmail" placeholder="">
+          <input  name="StudentName" type="text" class="form-control" id="stuName" placeholder="">
           <label for="student Name">Student Name</label>
         </div>
 
@@ -47,7 +75,7 @@
         <div class="form-floating mb-3">
 
             <!-- select -->
-            <select class="form-select" id="major">
+            <select required name="majorID" class="form-select" id="major">
                 <?php 
                     $sql = 'select * from major order by faculty';
                     $result= $conn->query($sql);
@@ -59,26 +87,28 @@
                 $conn->close(); 
                 ?>
             </select>
+
             <!-- <input type="select" class="form-control" id="floatingEmail" placeholder=""> -->
           <label for="Major">Major</label>
         </div>
 
         <!-- Password -->
         <div class="form-floating mb-3">
-          <input type="password" class="form-control" id="floatingEmail" placeholder="">
+          <input required name="password" type="password" class="form-control" id="password" placeholder="">
           <label for="password">Password</label>
         </div>
 
         <!-- Retype Password -->
         <div class="form-floating mb-3">
-          <input type="password" class="form-control" id="floatingEmail" placeholder="">
+          <input required type="password" class="form-control" id="re-password" placeholder="">
           <label for="Retype Password">Retype Password</label>
         </div>
 
         <!-- submit -->
-        <button class="btn btn-primary w-100 py-2 mb-3" type="submit">Sign up</button>
+        <button name="submit" class="btn btn-primary w-100 py-2 mb-3" type="submit">Sign up</button>
         <!-- <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p> -->
       </form>
+
     </main>
     <!-- Bootstrap script --> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
